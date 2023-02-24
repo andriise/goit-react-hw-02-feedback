57da2528de171d99ad5e43fe76faf386e3b48142
Andrii Marchuk <andrii.marchuck@gmail.com>
2023-02-24 17:47:49 +0200

    first commit

---
+++ src/components/Feedback/FeedbackField/FeedbackField.jsx
@@ -0,0 +1,12 @@
+export const FeedbackField = ({ onAddStats }) => {
+  return (
+    <section>
+      <h2>Please leave feedback</h2>
+      <div>
+        <button type="button">Good</button>
+        <button type="button">Neutral</button>
+        <button type="button">Bad</button>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Feedback/FeedbackStatistics/FeedbackStatistics.jsx
@@ -0,0 +1,12 @@
+export const FeedbackStatistics = () => {
+  return (
+    <section>
+      <h2>Statistics</h2>
+      <ul>
+        <li>Good: </li>
+        <li>Neutral: </li>
+        <li>Bad: </li>
+      </ul>
+    </section>
+  );
+};
